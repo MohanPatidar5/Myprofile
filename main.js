9af3e1c3d8a1607f75d550792a1b758172357147
Mohan Patidar <49092049+MohanPatidar5@users.noreply.github.com>
2024-06-16 21:12:48 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,54 @@
+// === MENU SHOW Y HIDDEN
+const navMenu = document.getElementById('nav-menu'),
+      toggleMenu = document.getElementById('nav-toggle'),
+      closeMenu = document.getElementById('nav-close')
+ 
+
+//SHOW
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+
+
+//HIDDEN
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+
+//REMOVE MENU
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+//SCROLL SECTION ACTIVE LINKS
+const sections = document.querySelectorAll('section[id]')
+
+//console.log("Hi mohan sections id-->", sections)
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+
+    const scrollY = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+
+        let sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.remove('active')
+        }
+    })
+
+
+}
